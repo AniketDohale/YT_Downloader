02bf935192c665b595ac959537d53dd44f1ca985
AniketDohale <aniket.dohale.2801@gmail.com>
2025-12-02 20:19:28 +0530

    Add Fetch button to get thumbnail and title

--- downloader/views.py
+++ downloader/views.py
@@ -7,22 +7,35 @@ def download_Video(request):
         del request.session['download_success']
 
     download_Success = False
+    video_Info = None
+    
     if request.method == 'POST':
         url = request.POST.get('url')
+        action = request.POST.get('action')
+        print(action)
         
         if url:
             ydl_opts = {
                 'format' : 'best',
                 'outtmpl' : 'downloads/%(title)s.%(ext)s',
+                'quiet' : True,
             }
         
         try:
             with YoutubeDL(ydl_opts) as ydl:
-                ydl.download([url])
-            download_Success = True
+                if action == 'fetch':
+                    info_Dict = ydl.extract_info(url, download=False)
+                    video_Info = {
+                        'title' : info_Dict.get('title'),
+                        'thumbnail' : info_Dict.get('thumbnail'),
+                    }
+
+                elif action == 'download':
+                   ydl.download([url])
+                   download_Success = True
 
         except Exception as e:
             print(f"Error Downloading Video {e}")
             download_Success = False
     
-    return render(request, 'index.html', {'download_Success': download_Success})
+    return render(request, 'index.html', {'video_Info': video_Info, 'download_Success': download_Success})
